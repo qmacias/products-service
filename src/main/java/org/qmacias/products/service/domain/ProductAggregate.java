d05bc61a8e3c260f6150644e2308736b3aedd8e7
qmacias <ezequielqmacias@gmail.com>
2023-02-19 15:55:08 -0300

    Add Google Guava/Add Aggregate Event Source Handler

--- src/main/java/org/qmacias/products/service/domain/ProductAggregate.java
+++ src/main/java/org/qmacias/products/service/domain/ProductAggregate.java
@@ -1,16 +1,30 @@
 package org.qmacias.products.service.domain;
 
+import org.axonframework.eventsourcing.EventSourcingHandler;
+import org.qmacias.products.service.core.event.ProductCreatedEvent;
+import org.qmacias.products.service.application.cmd.CreateProductCommand;
+
+import org.axonframework.spring.stereotype.Aggregate;
 import org.axonframework.commandhandling.CommandHandler;
 import org.axonframework.modelling.command.AggregateLifecycle;
-import org.axonframework.spring.stereotype.Aggregate;
-import org.qmacias.products.service.application.cmd.CreateProductCommand;
-import org.qmacias.products.service.core.event.ProductCreatedEvent;
+import org.axonframework.modelling.command.AggregateIdentifier;
+
 import org.springframework.beans.BeanUtils;
 
 import java.math.BigDecimal;
+import java.math.BigInteger;
 
 @Aggregate
 public class ProductAggregate {
+
+    @AggregateIdentifier
+    private String productId;
+
+    private String title;
+
+    private BigDecimal price;
+
+    private BigInteger quantity;
 
     protected ProductAggregate() {
     }
@@ -31,6 +45,14 @@ public class ProductAggregate {
         BeanUtils.copyProperties(createProductCommand, productCreatedEvent);
 
         AggregateLifecycle.apply(productCreatedEvent);
+    }
+
+    @EventSourcingHandler
+    public void on(final ProductCreatedEvent productCreatedEvent) {
+        this.productId = productCreatedEvent.getProductId();
+        this.price = productCreatedEvent.getPrice();
+        this.title = productCreatedEvent.getTitle();
+        this.quantity = productCreatedEvent.getQuantity();
     }
 
 }
